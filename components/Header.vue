dd3bba742e13b3b46d31386195e7c9d5bf2ee7c6
Mustazico <your-email@example.com>
2025-06-06 00:27:28 +0200

    logout-btn

--- components/Header.vue
+++ components/Header.vue
@@ -3,8 +3,10 @@
         <div class="title">
             Lien Småbruk
         </div>
-      
-    </div>
+        <button class="button">
+          Logout
+        </button>
+      </div>
   </template>
   
   <script setup>
@@ -13,6 +15,9 @@
   
   <style scoped>
     .header-border {
+      display: flex;
+      justify-content: space-between;
+      position: relative;
       width: 100%;
       height: 100px;
       margin-bottom: 20px;
@@ -20,15 +25,36 @@
     
     }
 
-
     .title {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
         text-align: center;
         font-size: 100px;
         color: #D1E9F6;
         font-family: 'Inconsolata', 'sans-serif';
         font-weight: 800;
         text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.5);
-; 
    }
+
+   .button {
+    height: 50px;
+    align-self: center;
+    margin-left: auto;
+    padding: 10px 30px;
+    font-size: 16px;
+    background-color: #D1E9F6;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    font-family: 'Inconsolata', sans-serif;
+    font-weight: 600;
+    font-size: 22px;
+}
+
+.button:hover {
+    background-color: #b0d4e3; /* Add hover effect */
+}
   </style>
   
